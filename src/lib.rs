56c0af79c51e50c2ce185e2499ead6591a68fc67
enuesaa <nsrciog@gmail.com>
2022-09-24 00:44:52 +0900

    fix: xhange code to check wasm is working

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,13 @@ use wasm_bindgen::prelude::*;
 pub mod json;
 pub mod yaml;
 
-use crate::json::serializer::Serializer;
-use crate::yaml::deserializer::Deserializer;
-
 #[wasm_bindgen]
-pub fn greet(value: &str) {
-  let mut serializer = Serializer::new();
-  serializer.serialize(&value);
+pub fn greet(value: &str) -> String {
+  // let mut serializer = Serializer::new();
+  // serializer.serialize(&value);
 
-  let values = serializer.values.clone();
-  let mut deserializer = Deserializer::new(values);
-  deserializer.print();
+  // let values = serializer.values.clone();
+  // let mut deserializer = Deserializer::new(values);
+  // deserializer.print();
+  String::from("a") + value
 }
