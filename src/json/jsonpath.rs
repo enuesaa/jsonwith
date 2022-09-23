56c40d7c9484c0e6c90af9341fa22b61cbfcdc83
enuesaa <nsrciog@gmail.com>
2022-09-23 17:36:19 +0900

    refactor: json path struct

--- src/json/jsonpath.rs
+++ src/json/jsonpath.rs
@@ -1,73 +1,69 @@
 #[derive(Debug, Clone, PartialEq)]
+struct JsonPathIndicator {
+    indicate: String,
+    count: usize,
+}
+impl JsonPathIndicator {
+    fn add_count(&mut self) {
+        self.count += 1;
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct JsonPath {
     pub value: Vec<String>, // [".", ".aaa", "[]", "[0]."]
-    list_i_vec: Vec<usize>,
-    list_i_type: Vec<String>,
+    indicators: Vec<JsonPathIndicator>,
 }
 impl JsonPath {
     pub fn new() -> Self {
-        JsonPath {
-            value: Vec::new(),
-            list_i_vec: Vec::new(),
-            list_i_type: Vec::new(),
-        }
+        JsonPath {value: Vec::new(), indicators: Vec::new()}
     }
 
     pub fn start_dict(&mut self) {
         self.add_something_item();
-        self.list_i_vec.push(0);
-        self.list_i_type.push("dict".to_string());
+        self.indicators.push(JsonPathIndicator{indicate: String::from("dict"), count: 0});
         self.value.push(".".to_string());
     }
 
     pub fn add_dict_key(&mut self, value: String) {
-        let last = self.list_i_vec.last().unwrap().clone();
-        self.list_i_vec.pop();
-        self.list_i_vec.push(last + 1);
-
-        let converted = Vec::from([".".to_string(), value]);
-        self.value.pop();
-        self.value.push(converted.join(""));
+        if let Some(last) = self.indicators.last_mut() {
+            last.add_count();
+        }
+        if let Some(last_value) = self.value.last_mut() {
+            *last_value = String::from(".") + &value;
+        }
     }
 
     pub fn end_dict(&mut self) {
         self.value.pop();
-        self.list_i_vec.pop();
-        self.list_i_type.pop();
+        self.indicators.pop();
     }
 
     pub fn start_list(&mut self) {
-        self.list_i_vec.push(0);
-        self.list_i_type.push("list".to_string());
-
-        self.value.push("[]".to_string());
+        self.indicators.push(JsonPathIndicator{indicate: String::from("list"), count: 0});
+        self.value.push(String::from("[]"));
     }
 
     pub fn add_something_item(&mut self) {
-        if self.list_i_type.last() == None {
-            return;
-        }
-        let itemtype = self.list_i_type.last().unwrap().clone();
-        if itemtype == "list".to_string() {
+        let last = self.indicators.last();
+        if last != None && last.unwrap().indicate == String::from("list") {
             self.add_list_item();
         }
         // dict の場合は add_dict_key() で既にキーが追加されている
     }
 
     pub fn add_list_item(&mut self) {
-        let last = self.list_i_vec.last().unwrap().clone();
-        self.list_i_vec.pop();
-        self.list_i_vec.push(last + 1);
-
-        let converted = Vec::from(["[".to_string(), last.to_string(), "]".to_string()]);
-        self.value.pop();
-        self.value.push(converted.join(""));
+        if let Some(last) = self.indicators.last_mut() {
+            if let Some(last_value) = self.value.last_mut() {
+                *last_value = String::from("[") + &last.count.to_string() + "]";
+            }
+            last.add_count();
+        }
     }
 
     pub fn end_list(&mut self) {
         self.value.pop();
-        self.list_i_vec.pop();
-        self.list_i_type.pop();
+        self.indicators.pop();
     }
 
     pub fn to_string(&mut self) -> String {
