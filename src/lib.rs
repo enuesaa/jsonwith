918822ab8ab0d9bc23203bf41b2f556d47271181
enuesaa <nsrciog@gmail.com>
2022-09-24 00:25:15 +0900

    feat: wasm で実行するためのコードを追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
+use wasm_bindgen::prelude::*;
+
 pub mod json;
 pub mod yaml;
+
+use crate::json::serializer::Serializer;
+use crate::yaml::deserializer::Deserializer;
+
+#[wasm_bindgen]
+pub fn greet(value: &str) {
+  let mut serializer = Serializer::new();
+  serializer.serialize(&value);
+
+  let values = serializer.values.clone();
+  let mut deserializer = Deserializer::new(values);
+  deserializer.print();
+}
