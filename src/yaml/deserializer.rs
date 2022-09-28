ff51db87e1a63892eaf59aa148dd7d0491925986
enuesaa <nsrciog@gmail.com>
2022-09-29 03:57:10 +0900

    refactor: yaml deserializer

--- src/yaml/deserializer.rs
+++ src/yaml/deserializer.rs
@@ -1,14 +1,15 @@
 use crate::json::path::{Path, JsonPathIndicator};
 use crate::json::value::Value;
 use crate::json::part::Part;
+use crate::yaml::line::Line;
 
 pub struct Deserializer {
-    pub yaml_string: String,
+    pub lines: Vec<Line>,
     spaces: usize,
 }
 impl Deserializer {
     pub fn new() -> Self {
-        Deserializer{yaml_string: String::from(""), spaces: 0}
+        Deserializer{lines: Vec::new(), spaces: 0}
     }
 
     pub fn deserialize(&mut self, values: Vec<Value>) -> String {
@@ -16,72 +17,81 @@ impl Deserializer {
             let path = value.path.clone();
             let part = value.part.clone();
             match part {
-                Part::StartDict => {self.resolve_start_dict(path)},
-                Part::EndDict => {self.resolve_end_dict(path)},
-                Part::StartList => {self.resolve_start_list(path)},
-                Part::EndList => {self.resolve_end_list(path)},
-                _ => {self.resolve_others(path, part)}
+                Part::StartDict => {self.resolve_start_dict_(path)},
+                Part::EndDict => {self.resolve_end_dict_(path)},
+                Part::StartList => {self.resolve_start_list_(path)},
+                Part::EndList => {self.resolve_end_list_(path)},
+                _ => {self.resolve_others_(path, part)}
             }
         }
-        self.yaml_string.clone()
+        self.lines.iter().map(|v| v.to_string()).collect()
     }
 
-    fn resolve_others(&mut self, mut path: Path, part: Part) {
+    fn resolve_others_(&mut self, mut path: Path, part: Part) {
         let indicator = &path.get_last_indicator();
         let key = path.value[path.value.len() - 1].clone();
-        self.resolve_key(indicator, key);
-        self.yaml_string += &format!("{}\n", part);
+        let mut line = self.resolve_key_(indicator, key);
+        line.set_value(part.to_string());
+        self.lines.push(line);
     }
 
-    fn resolve_start_dict(&mut self, mut path: Path) {
+    fn resolve_start_dict_(&mut self, mut path: Path) {
         if !path.is_root() {
             let indicator = &path.get_previous_indicator();
             let key = path.value[path.value.len() - 2].clone();
-            self.resolve_key(indicator, key);
-            if indicator.indicate == *"dict" {
-                self.yaml_string += "\n";
-            }
+            let line = self.resolve_key_(indicator, key);
+            self.lines.push(line);
             self.spaces += 2;
         }
     }
 
-    fn resolve_end_dict(&mut self, mut path: Path) { 
+    fn resolve_end_dict_(&mut self, mut path: Path) {
         let indicator = &path.get_last_indicator();
         if indicator.count == 0 {
-            self.yaml_string.pop();
-            self.yaml_string += "{}\n";
+            if let Some(line) = self.lines.last_mut() {
+                line.enable_empty_dict_blancket();
+            }
         }
         if !path.is_root() {
             self.spaces -= 2;
         }
     }
 
-    fn resolve_start_list(&mut self, mut path: Path) {
+    fn resolve_start_list_(&mut self, mut path: Path) {
         if !path.is_root() {
             let indicator = &path.get_previous_indicator();
             let key = path.value[path.value.len() - 2].clone();
-            self.resolve_key(indicator, key);
-            if indicator.indicate == *"dict" {
-                self.yaml_string += "\n";
+            let line = self.resolve_key_(indicator, key);
+            self.lines.push(line);
+        }
+    }
+
+    fn resolve_end_list_(&mut self, mut path: Path) { 
+        let indicator = &path.get_last_indicator();
+        if indicator.count == 0 {
+            if let Some(line) = self.lines.last_mut() {
+                line.enable_empty_list_blancket();
             }
         }
     }
 
-    fn resolve_end_list(&mut self, mut path: Path) {
-        let indicator = &path.get_last_indicator();
-        if indicator.count == 0 {
-            self.yaml_string.pop();
-            self.yaml_string += "[]\n";
+    fn resolve_key_(&mut self, indicator: &JsonPathIndicator, key: String) -> Line {
+        let mut line :Line;
+        if self.lines.len() > 1 && self.lines.last_mut().unwrap().is_hyphen_only() {
+            line = self.lines.last_mut().unwrap().clone();
+            self.lines.pop();
+        } else {
+            line = Line::new();
         }
-    }
-
-    fn resolve_key(&mut self, indicator: &JsonPathIndicator, key: String) {
         if indicator.indicate == *"dict" {
-            let need_spaces = self.spaces - self.yaml_string.split("\n").last().unwrap_or("").as_bytes().len();
-            self.yaml_string += &format!("{}{}: ", " ".repeat(need_spaces), &key[1..]);
+            line.set_spaces(if line.need_hyphen { self.spaces - 2 } else { self.spaces });
+            line.set_key(key[1..].to_string());
+            line.enable_colon();
         } else if indicator.indicate == *"list" {
-            self.yaml_string += &format!("{}- ", " ".repeat(self.spaces));
+            line.set_spaces(self.spaces);
+            line.enable_hyphen();
         }
+        line
     }
 
     pub fn print_debug(&mut self, values: Vec<Value>) {
---
+++ src/yaml/line.rs
@@ -0,0 +1,79 @@
+use std::fmt;
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Line {
+  spaces: usize,
+  pub need_hyphen: bool,
+  key: String,
+  need_colon: bool,
+  value: String,
+  need_empty_dict_blancket: bool,
+  need_empty_list_blancket: bool,
+}
+impl Line {
+  pub fn new() -> Self {
+    Self { spaces: 0, need_hyphen: false, key: String::from(""), need_colon: false, value: String::from(""), need_empty_dict_blancket: false, need_empty_list_blancket: false }
+  }
+
+  pub fn set_spaces(&mut self, spaces: usize) {
+    self.spaces = spaces;
+  }
+  
+  pub fn enable_hyphen(&mut self) {
+    self.need_hyphen = true;
+  }
+  
+  pub fn disable_hyphen(&mut self) {
+    self.need_hyphen = false;
+  }
+
+  pub fn set_key(&mut self, key: String) {
+    self.key = key;
+  }
+
+  pub fn enable_colon(&mut self) {
+    self.need_colon = true;
+  }
+  
+  pub fn disable_colon(&mut self) {
+    self.need_colon = false;
+  }
+  
+  pub fn set_value(&mut self, value: String) {
+    self.value = value;
+  }
+
+  pub fn enable_empty_dict_blancket(&mut self) {
+    self.need_empty_dict_blancket = true;
+  }
+  
+  pub fn disable_empty_dict_blancket(&mut self) {
+    self.need_empty_dict_blancket = false;
+  }
+  
+  pub fn enable_empty_list_blancket(&mut self) {
+    self.need_empty_list_blancket = true;
+  }
+  
+  pub fn disable_empty_list_blancket(&mut self) {
+    self.need_empty_list_blancket = false;
+  }
+
+  pub fn is_hyphen_only(&mut self) -> bool {
+    self.need_hyphen == true && self.key == String::from("") && self.need_colon == false && self.value == String::from("") && self.need_empty_dict_blancket == false && self.need_empty_list_blancket == false
+  }
+}
+
+impl fmt::Display for Line {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}{}{}{}{}{}{}\n",
+      " ".repeat(self.spaces),
+      if self.need_hyphen {"- "} else {""},
+      self.key,
+      if self.need_colon {": "} else {""},
+      self.value,
+      if self.need_empty_dict_blancket {"{}"} else {""},
+      if self.need_empty_list_blancket {"[]"} else {""},
+    )
+  }
+}
