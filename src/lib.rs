17ae38c7fb2349b092e44629712873aff21737e6
enuesaa <nsrciog@gmail.com>
2023-02-26 18:11:05 +0900

    chore(deps): remove wasm-bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use wasm_bindgen::prelude::*;
-
 pub mod json;
 pub mod util;
 pub mod yaml;
@@ -8,8 +6,6 @@ use crate::json::deserializer::Deserializer as JsonDeserializer;
 use crate::json::serializer::Serializer as JsonSerializer;
 use crate::yaml::deserializer::Deserializer as YamlDeserializer;
 
-/* @see https://github.com/rustwasm/wasm-bindgen/issues/2882 */
-#[wasm_bindgen]
 pub fn json2yaml(value: &str, indent: usize) -> String {
     let mut serializer = JsonSerializer::new();
     let values = serializer.serialize(value);
@@ -19,7 +15,6 @@ pub fn json2yaml(value: &str, indent: usize) -> String {
     deserializer.deserialize(values)
 }
 
-#[wasm_bindgen]
 pub fn json2json(value: &str, indent: usize) -> String {
     let mut serializer = JsonSerializer::new();
     let values = serializer.serialize(value);
