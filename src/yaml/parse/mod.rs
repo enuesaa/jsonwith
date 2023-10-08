e08a38d2dc042cebff14d4694e978f568fca7213
enuesaa <nsrciog@gmail.com>
2023-10-08 18:47:50 +0900

    refactor(yaml2json): do not use context

--- src/yaml/parse/mod.rs
+++ src/yaml/parse/mod.rs
@@ -1,3 +1,2 @@
-pub mod context;
 pub mod parser;
 pub mod line;
--- src/yaml/parse/parser.rs
+++ src/yaml/parse/parser.rs
@@ -1,52 +1,56 @@
+use crate::data::kv::Kv;
 use crate::data::kvs::Kvs;
 use crate::data::path::Path;
 use crate::data::tokens::Tokens;
-use crate::yaml::parse::context::Context;
 use crate::yaml::parse::line::Line;
 
-pub struct Parser {}
+pub struct Parser {
+    kvs: Kvs,
+    last_indent: usize,
+}
 impl Parser {
     pub fn new() -> Self {
-        Parser {}
+        Parser {
+            kvs: Kvs::new(),
+            last_indent: 0,
+        }
     }
 
     pub fn parse(&mut self, text: &str) -> Kvs {
-        let mut context = Context::new();
-        self.push_root_mkdict(&mut context);
+        self.push_root_mkdict();
 
         let mut line = Line::new();
         for c in text.chars() {
-            println!("{:?}", c);
             if line.is_ended() {
-                println!("ended");
-                self.push_context(&mut context, line);
+                self.push_context(line);
                 line = Line::new();
             }
             line.push(c);
         }
-        self.push_context(&mut context, line);
-        self.push_root_enddict(&mut context);
+        self.push_context(line);
+        self.push_root_enddict();
 
-        context.get_kvs()
+        self.kvs.clone()
     }
 
-    fn push_context(&self, context: &mut Context, line: Line) {
-        let mut path = context.get_last_path();
+    fn push_context(&mut self, line: Line) {
+        let mut path = self.get_last_path();
 
-        let last_indent = context.get_last_indent();
+        let last_indent = self.get_last_indent();
+        if last_indent >= line.get_indent() {
+            path.pop();
+        }
         if last_indent > line.get_indent() {
-            path.pop();
-            context.push(path.clone(), Tokens::EndDict);
-        } else if last_indent < line.get_indent() {
-            path.push(&line.get_key());
-        } else {
-            path.pop();
+            self.push(path.clone(), Tokens::EndDict);
+        }
+        if last_indent <= line.get_indent() {
             path.push(&line.get_key());
         }
-        
+
+        self.set_last_indent(line.get_indent());
 
         if !line.has_value() {
-            context.push(path, Tokens::MkDict);
+            self.push(path, Tokens::MkDict);
             return;
         }
 
@@ -64,14 +68,38 @@ impl Parser {
                 }
             },
         };
-        context.push(path, value);
+        self.push(path, value);
     }
 
-    fn push_root_mkdict(&self, context: &mut Context) {
-        context.push(Path::new(), Tokens::MkDict);
+    fn push_root_mkdict(&mut self) {
+        self.push(Path::new(), Tokens::MkDict);
     }
 
-    fn push_root_enddict(&self, context: &mut Context) {
-        context.push(Path::new(), Tokens::EndDict);
+    fn push_root_enddict(&mut self) {
+        if self.get_last_indent() > 0 {
+            let mut path = self.get_last_path();
+            path.pop();
+            self.push(path, Tokens::EndDict);
+        }
+        self.push(Path::new(), Tokens::EndDict);
+    }
+
+    fn push(&mut self, path: Path, value: Tokens) {
+        self.kvs.push(Kv::with(path, value));
+    }
+
+    fn get_last_path(&self) -> Path {
+        if let Some(last) = self.kvs.list().last() {
+            return last.get_path();
+        };
+        Path::new()
+    }
+
+    fn get_last_indent(&self) -> usize {
+        self.last_indent.clone()
+    }
+
+    fn set_last_indent(&mut self, indent: usize) {
+        self.last_indent = indent;
     }
 }
